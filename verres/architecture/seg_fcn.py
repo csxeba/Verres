931b2419a152c385f0ad9a70fd7e67335632e07a
csxeba <csxeba@gmail.com>
2019-05-18 16:26:41 +0200

    Getting ready for training

--- verres/architecture/seg_fcn.py
+++ verres/architecture/seg_fcn.py
@@ -5,67 +5,67 @@ def build(num_output_classes, onehot_y=False):
     inputs = keras.layers.Input((200, 320, 3))
 
     down_stage1 = keras.layers.Conv2D(8, 3, padding="same")(inputs)
-    # down_stage1 = keras.layers.BatchNormalization()(down_stage1)
+    down_stage1 = keras.layers.BatchNormalization()(down_stage1)
     down_stage1 = keras.layers.ReLU()(down_stage1)
 
     down_stage2 = keras.layers.MaxPool2D()(down_stage1)  # 100
     down_stage2 = keras.layers.Conv2D(8, 3, padding="same")(down_stage2)
-    # down_stage2 = keras.layers.BatchNormalization()(down_stage2)
+    down_stage2 = keras.layers.BatchNormalization()(down_stage2)
     down_stage2 = keras.layers.ReLU()(down_stage2)
 
     down_stage3 = keras.layers.MaxPool2D()(down_stage2)  # 50
     down_stage3 = keras.layers.Conv2D(16, 3, padding="same")(down_stage3)
-    # down_stage3 = keras.layers.BatchNormalization()(down_stage3)
+    down_stage3 = keras.layers.BatchNormalization()(down_stage3)
     down_stage3 = keras.layers.ReLU()(down_stage3)
 
     down_stage4 = keras.layers.MaxPool2D()(down_stage3)  # 25
     down_stage4 = keras.layers.Conv2D(16, (2, 1), padding="valid")(down_stage4)  # 24
-    # down_stage4 = keras.layers.BatchNormalization()(down_stage4)
+    down_stage4 = keras.layers.BatchNormalization()(down_stage4)
     down_stage4 = keras.layers.ReLU()(down_stage4)
 
     down_stage5 = keras.layers.MaxPool2D()(down_stage4)  # 12
     down_stage5 = keras.layers.Conv2D(16, 3, padding="same")(down_stage5)
-    # down_stage5 = keras.layers.BatchNormalization()(down_stage5)
+    down_stage5 = keras.layers.BatchNormalization()(down_stage5)
     down_stage5 = keras.layers.ReLU()(down_stage5)
 
     down_stage6 = keras.layers.MaxPool2D()(down_stage5)  # 6, 10
     down_stage6 = keras.layers.Conv2D(16, 3, padding="same")(down_stage6)
-    # down_stage6 = keras.layers.BatchNormalization()(down_stage6)
+    down_stage6 = keras.layers.BatchNormalization()(down_stage6)
     down_stage6 = keras.layers.ReLU()(down_stage6)
 
     down_stage7 = keras.layers.MaxPool2D()(down_stage6)  # 3, 5
     down_stage7 = keras.layers.Conv2D(32, 3, padding="same")(down_stage7)
-    # down_stage7 = keras.layers.BatchNormalization()(down_stage7)
+    down_stage7 = keras.layers.BatchNormalization()(down_stage7)
     down_stage7 = keras.layers.ReLU()(down_stage7)
 
     x = keras.layers.UpSampling2D()(down_stage7)  # 6, 10
     x = keras.layers.concatenate([down_stage6, x])
     x = keras.layers.Conv2D(32, 3, padding="same")(x)
-    # x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.BatchNormalization()(x)
     x = keras.layers.ReLU()(x)
 
     x = keras.layers.UpSampling2D()(x)  # 12, 20
     x = keras.layers.concatenate([down_stage5, x])
     x = keras.layers.Conv2D(16, 3, padding="same")(x)
-    # x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.BatchNormalization()(x)
     x = keras.layers.ReLU()(x)
 
     x = keras.layers.UpSampling2D()(x)  # 24, 40
     x = keras.layers.concatenate([down_stage4, x])
     x = keras.layers.Conv2DTranspose(16, (2, 1), padding="valid")(x)  # 25, 40
-    # x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.BatchNormalization()(x)
     x = keras.layers.ReLU()(x)
 
     x = keras.layers.UpSampling2D()(x)  # 50, 80
     x = keras.layers.concatenate([down_stage3, x])
     x = keras.layers.Conv2D(16, 3, padding="same")(x)
-    # x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.BatchNormalization()(x)
     x = keras.layers.ReLU()(x)
 
     x = keras.layers.UpSampling2D()(x)  # 100, 160
     x = keras.layers.concatenate([down_stage2, x])
     x = keras.layers.Conv2D(8, 3, padding="same")(x)
-    # x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.BatchNormalization()(x)
     x = keras.layers.ReLU()(x)
 
     x = keras.layers.UpSampling2D()(x)  # 200, 320
--- verres/artifactory.py
+++ verres/artifactory.py
@@ -1,26 +1,27 @@
+import os
 import pathlib
 import datetime
 
 
 class Artifactory:
 
-    def __init__(self):
-        current = pathlib.Path.cwd()
-        if os.path.join("verres", "experiments") in os.getcwd():
+    def __init__(self, root=None):
+        if root is None:
+            root = self._default_root()
+        self.root = pathlib.Path(root)
+        self.experiment_root = self.root/datetime.datetime.now().strftime("xp_%Y%m%d.%H%M%S")
+        self.checkpoint_root = self.experiment_root/"checkpoints"
+        self.tensorboard_root = self.experiment_root/"tensorboard"
+        for roots in [self.checkpoint_root, self.tensorboard_root]:
+            roots.mkdir(parents=True, exist_ok=True)
+        self.logfile_path = self.experiment_root/"training_logs.csv"
+
+    @staticmethod
+    def _default_root():
+        current = os.getcwd()
+        if "experiments" in current:
             os.chdir("..")
-        self.root = os.path.abspath("./artifactory")
-        self.experiment_root = os.path.join(self.root, datetime.datetime.now().strftime("xp_%Y%m%d.%H%M%S"))
-        self.checkpoint_root = os.path.join(self.experiment_root, "checkpoints")
-        self.tensorboard_root = os.path.join(self.experiment_root, "tensorboard")
-        os.makedirs("artifactory")
-
-    @property
-    def tensorboard_root(self):
-        return
-
-    @property
-    def logfile_path(self):
-        return os.path.join(self.experiment_root, "training_logs.csv")
+        return os.path.join(current, "artifactory")
 
     def make_checkpoint_template(self, model_name=""):
         return os.path.join(self.checkpoint_root, "{}_chkp_{}".format(model_name, "{}"))
