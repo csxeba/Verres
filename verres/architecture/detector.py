f2930dc495b882e4306199161519a754978f40ef
csxeba <csxeba@gmail.com>
2019-06-09 16:29:06 +0200

    More versatile detector arch

--- verres/architecture/detector.py
+++ verres/architecture/detector.py
@@ -3,48 +3,49 @@ import tensorflow as tf
 from ..utils import losses
 
 
+def _block(x0, num_filters, depth, pool):
+    if pool:
+        x0 = tf.keras.layers.MaxPool2D()(x0)
+    x = tf.keras.layers.Conv2D(num_filters, 3, padding="same")(x0)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.ReLU()(x)
+    for d in range(depth-1):
+        x = tf.keras.layers.Conv2D(num_filters, 3, padding="same")(x0)
+        x = tf.keras.layers.BatchNormalization()(x)
+        x = tf.keras.layers.ReLU()(x)
+    if depth > 1:
+        x = tf.keras.layers.Add()([x0, x])
+    return x
+
+
+def _pool(x):
+    return tf.keras.layers.MaxPool2D()(x)
+
+
 class COCODoomDetector:
 
-    def __init__(self, input_shape="cocodoom", num_output_classes=18):
+    def __init__(self, input_shape: tuple="cocodoom", num_output_classes=18, block_depth=1, block_widening=1):
 
-        if len(input_shape) == 3:
-            input_shape = (None,) + input_shape
         if input_shape == "cocodoom":
             input_shape = (None, 200, 320, 3)
+        if len(input_shape) == 3:
+            input_shape = (None,) + input_shape
 
         inputs = tf.keras.layers.Input(batch_shape=input_shape, name="images")
 
-        down_stage1 = tf.keras.layers.Conv2D(8, 3, padding="same")(inputs)
-        down_stage1 = tf.keras.layers.BatchNormalization()(down_stage1)
-        down_stage1 = tf.keras.layers.ReLU()(down_stage1)
+        x = _block(inputs, 8*block_widening, block_depth, 0)  # 200 320
+        x = _block(x, 16*block_widening, block_depth, 1)  # 100 160
+        x = _block(x, 32*block_widening, block_depth, 1)  # 50 80
+        x = _block(x, 64*block_widening, block_depth, 1)  # 25 40
 
-        down_stage2 = tf.keras.layers.MaxPool2D()(down_stage1)  # 100
-        down_stage2 = tf.keras.layers.Conv2D(16, 3, padding="same")(down_stage2)
-        down_stage2 = tf.keras.layers.BatchNormalization()(down_stage2)
-        down_stage2 = tf.keras.layers.ReLU()(down_stage2)
-
-        down_stage3 = tf.keras.layers.MaxPool2D()(down_stage2)  # 50
-        down_stage3 = tf.keras.layers.Conv2D(16, 3, padding="same")(down_stage3)
-        down_stage3 = tf.keras.layers.BatchNormalization()(down_stage3)
-        down_stage3 = tf.keras.layers.ReLU()(down_stage3)
-
-        down_stage4 = tf.keras.layers.MaxPool2D()(down_stage3)  # 25
-        down_stage4 = tf.keras.layers.Conv2D(16, 3, padding="same")(down_stage4)
-        down_stage4 = tf.keras.layers.BatchNormalization()(down_stage4)
-        down_stage4 = tf.keras.layers.ReLU()(down_stage4)
-
-        down_stage4 = tf.keras.layers.Conv2D(16, 3, padding="same")(down_stage4)
-        down_stage4 = tf.keras.layers.BatchNormalization()(down_stage4)
-        down_stage4 = tf.keras.layers.ReLU()(down_stage4)
-
-        mask = tf.keras.Input(batch_shape=tf.keras.backend.int_shape(down_stage4)[:3] + (2,))
+        mask = tf.keras.Input(batch_shape=tf.keras.backend.int_shape(x)[:3] + (2,))
 
         heatmap_output = tf.keras.layers.Conv2D(num_output_classes, kernel_size=5, padding="same",
-                                                name="Heatmaps")(down_stage4)
+                                                name="Heatmaps")(x)
         refinement_output = tf.keras.layers.Conv2D(2, kernel_size=5, padding="same",
-                                                   name="Refinements")(down_stage4)
+                                                   name="Refinements")(x)
         boxparam_output = tf.keras.layers.Conv2D(2, kernel_size=5, padding="same",
-                                                 name="BoxParams")(down_stage4)
+                                                 name="BoxParams")(x)
 
         masked_refinement = tf.keras.layers.Lambda(lambda xx: xx[0] * xx[1],
                                                    name="Masked_refinements")([refinement_output, mask])
