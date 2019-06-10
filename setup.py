aac6ddfce6ef0a0916fbd79b466374b9c8f1a088
csxeba <csxeba@gmail.com>
2019-06-10 22:04:42 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='verres',
+    version='0.0.1',
+    packages=find_packages(),
+    url='https://github.com/csxeba/Verres.git',
+    license='MIT',
+    author='Csxeba',
+    author_email='csxeba@gmail.com',
+    description='Curiosity'
+)
